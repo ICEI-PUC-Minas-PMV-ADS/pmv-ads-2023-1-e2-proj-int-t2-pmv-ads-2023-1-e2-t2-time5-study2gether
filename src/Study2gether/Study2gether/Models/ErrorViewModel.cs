39d35ea5257649a1242c016041f3eae9f87b94b4
sh1nj12code <sh1nj12code@gmail.com>
2023-04-15 12:14:55 -0300

    [feature] Readicionada Base da Aplicação ASP.NET Core(Model-View-Controller)

---
+++ src/Study2gether/Study2gether/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Study2gether.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
