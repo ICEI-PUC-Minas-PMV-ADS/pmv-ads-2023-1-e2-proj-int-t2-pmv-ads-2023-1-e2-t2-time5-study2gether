b0fb2826d01c40cafbe13ed908b1b7ce7e0e0da3
Elpidio / Nick: (TheBlackCake) <lomeuelpidio@gmail.com>
2023-04-16 15:22:50 -0300

    feature: entity configuration
    https://trello.com/c/2QQy8aYr/69-configuração-padrão-do-entity

--- src/Study2gether/Study2gether/Startup.cs
+++ src/Study2gether/Study2gether/Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Study2gether.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +25,9 @@ namespace Study2gether
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+            );
             services.AddControllersWithViews();
         }
 
