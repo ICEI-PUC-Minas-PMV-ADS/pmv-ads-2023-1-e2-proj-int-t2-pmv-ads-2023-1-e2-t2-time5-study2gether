d931f14aaaff7a7ecf9e84534f4b5b2d11a61d0f
Elpidio / Nick: (TheBlackCake) <lomeuelpidio@gmail.com>
2023-04-16 19:27:16 -0300

    feature: Users table  (by Odair)
    https://trello.com/c/ntyi3Tkl/61-etapa-3-programação-de-funcionalidades-db-users

---
+++ src/Study2gether/Study2gether/Controllers/UsersController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Study2gether.Models;
+
+namespace Study2gether.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UsersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Users
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Users.ToListAsync());
+        }
+
+        // GET: Users/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users
+                .FirstOrDefaultAsync(m => m.idUser == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // GET: Users/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Users/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("idUser,name,password,email,description,imageLink,socialMedia,createdDate,isAdmin")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                user.password = BCrypt.Net.BCrypt.HashPassword(user.password);
+                user.idUser = Guid.NewGuid();
+                _context.Add(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+        // GET: Users/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("idUser,name,password,email,description,imageLink,socialMedia,createdDate,isAdmin")] User user)
+        {
+            if (id != user.idUser)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    user.password = BCrypt.Net.BCrypt.HashPassword(user.password);
+                    _context.Update(user);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserExists(user.idUser))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+        // GET: Users/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users
+                .FirstOrDefaultAsync(m => m.idUser == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserExists(Guid id)
+        {
+            return _context.Users.Any(e => e.idUser == id);
+        }
+    }
+}
---
+++ src/Study2gether/Study2gether/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Study2gether.Models
+{
+    public class User
+    {
+        [Key]
+
+        public Guid idUser { get; set; }
+
+        public string name { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório Informar a Senha!")]
+
+        [DataType(DataType.Password)]
+        public string password { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório Informar a Email!")]
+
+        public string email { get; set; }
+
+        public string description { get; set; }
+
+        public string imageLink { get; set; }
+
+        public string socialMedia { get; set; }
+
+        public DateTime createdDate { get; set; }
+
+        public bool isAdmin { get; set; }
+    }
+
+}
