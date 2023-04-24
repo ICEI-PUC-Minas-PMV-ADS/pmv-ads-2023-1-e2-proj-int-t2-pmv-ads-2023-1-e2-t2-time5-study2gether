2a39c916e9a36b284c3cd9f21defdb8936941eff
OdairMarra <114453508+OdairMarra@users.noreply.github.com>
2023-04-24 20:32:14 -0300

    Feature: Alteracao Login

--- src/Study2gether/Study2gether/Controllers/UsersController.cs
+++ src/Study2gether/Study2gether/Controllers/UsersController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -27,26 +28,26 @@ namespace Study2gether.Controllers
 
         [HttpPost]
 
-        public async Task<IActionResult> Login([Bind("email,password")] User user)
+        public async Task<IActionResult> Login([Bind("email,password")] User userForm)
         {
-            var usuario = await _context.Users
-                .FirstOrDefaultAsync(m => m.email == user.email);
+            var userDb = await _context.Users
+                .FirstOrDefaultAsync(m => m.email == userForm.email);
 
-            if (usuario == null)
+            if (userDb == null)
             {
                 ViewBag.Message = "Usuário e/ou Senha inválidos!";
                 return View();
             }
 
-            bool ispasswordOk = BCrypt.Net.BCrypt.Verify(user.password, user.password);
+            bool ispasswordOk = BCrypt.Net.BCrypt.Verify(userForm.password, userDb.password);
 
             if (ispasswordOk)
             {
                 var claims = new List<Claim>
-                {
-                    new Claim(ClaimTypes.Name, user.name),
-                    new Claim(ClaimTypes.NameIdentifier, user.name),                  
-                };
+                    {
+                        new Claim(ClaimTypes.Name, userDb.email),
+                        new Claim(ClaimTypes.NameIdentifier, userDb.email),
+                    };
 
                 var userIdentity = new ClaimsIdentity(claims, "login");
 
@@ -55,7 +56,7 @@ namespace Study2gether.Controllers
                 var props = new AuthenticationProperties
                 {
                     AllowRefresh = true,
-                    ExpiresUtc = DateTime.Now.ToLocalTime().AddDays(1),
+                    ExpiresUtc = DateTime.Now.ToLocalTime().AddDays(7),
                     IsPersistent = true
                 };
 
@@ -68,10 +69,12 @@ namespace Study2gether.Controllers
             ViewBag.Message = "Usuário e/ou Senha inválidos!";
             return View();
         }
+
+        [AllowAnonymous]
         public async Task<IActionResult> Logout() 
         {
             await HttpContext.SignOutAsync();
-            return RedirectToAction("Login", "User");
+            return RedirectToAction("Index", "Home");
         }
 
 
