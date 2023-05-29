3e01636665f9e3864f7632335966176ba24ace56
Elpidio / Nick: (TheBlackCake) <lomeuelpidio@gmail.com>
2023-05-29 17:01:21 -0300

    feature: recuperação de senha

---
+++ src/Study2gether/Study2gether/Migrations/20230528150836_ForgotPassword-02.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Study2gether.Migrations
+{
+    public partial class ForgotPassword02 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ForgotPassword",
+                columns: table => new
+                {
+                    idForgotPass = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    emailSent = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ForgotPassword", x => x.idForgotPass);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ForgotPassword");
+        }
+    }
+}
--- src/Study2gether/Study2gether/Migrations/20230528153708_ForgotPassword-03.Designer.cs
+++ src/Study2gether/Study2gether/Migrations/20230528153708_ForgotPassword-03.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Study2gether.Models;
 
 namespace Study2gether.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230528153708_ForgotPassword-03")]
+    partial class ForgotPassword03
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
