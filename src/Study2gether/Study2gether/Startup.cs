439c7f9a0c2b19667e81385b10010739d33ef947
Elpidio / Nick: (TheBlackCake) <lomeuelpidio@gmail.com>
2023-05-29 11:39:17 -0300

    feature: arquivos de configuração para deploy.
    ainda tenho que verificar uns pontos, mas ok por hora.

--- src/Study2gether/Study2gether/Startup.cs
+++ src/Study2gether/Study2gether/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Study2gether.Atributtes;
 using Study2gether.Models;
 using System;
@@ -25,32 +26,38 @@ namespace Study2gether
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+            var connectionString = "Server=MYSQL5044.site4now.net;Database=db_a9a07d_studytw;Uid=a9a07d_studytw;Pwd=f4487dhwyfdTSDYF";
+            var serverVersion = new MySqlServerVersion(new Version(8, 0, 31));
+            services.AddDbContext<ApplicationDbContext>(
+                dbContextOptions => dbContextOptions
+                    .UseMySql(connectionString, serverVersion)
+                    .LogTo(Console.WriteLine, LogLevel.Information)
+                    .EnableSensitiveDataLogging()
+                    .EnableDetailedErrors()
             );
 
             services.Configure<CookiePolicyOptions>(options =>
             {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
+                    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                    options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
             services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-                .AddCookie(options => {
+                .AddCookie(options =>
+                {
                     options.AccessDeniedPath = "/user/AccessDenied/";
-                    options.LoginPath = "/user/Login/";                 
+                    options.LoginPath = "/user/Login/";
                 });
 
-        services.AddControllersWithViews(options =>
-        {
-            options.Filters.Add<ViewBagActionFilter>();
-        });
+            services.AddControllersWithViews(options =>
+            {
+                options.Filters.Add<ViewBagActionFilter>();
+            });
         }
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
