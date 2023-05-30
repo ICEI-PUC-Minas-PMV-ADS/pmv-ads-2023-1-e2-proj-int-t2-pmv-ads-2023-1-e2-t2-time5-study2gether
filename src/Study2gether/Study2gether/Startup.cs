5663b9e1576da905664327e811d3a18c5e85f096
Elpidio / Nick: (TheBlackCake) <lomeuelpidio@gmail.com>
2023-05-30 13:29:33 -0300

    feature: database change

--- src/Study2gether/Study2gether/Startup.cs
+++ src/Study2gether/Study2gether/Startup.cs
@@ -26,38 +26,64 @@ namespace Study2gether
 
         public IConfiguration Configuration { get; }
 
-
+        //Banco de dados local
         public void ConfigureServices(IServiceCollection services)
         {
-            var connectionString = "Server=MYSQL5044.site4now.net;Database=db_a9a07d_studytw;Uid=a9a07d_studytw;Pwd=f4487dhwyfdTSDYF";
-            var serverVersion = new MySqlServerVersion(new Version(8, 0, 31));
-            services.AddDbContext<ApplicationDbContext>(
-                dbContextOptions => dbContextOptions
-                    .UseMySql(connectionString, serverVersion)
-                    .LogTo(Console.WriteLine, LogLevel.Information)
-                    .EnableSensitiveDataLogging()
-                    .EnableDetailedErrors()
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
             );
 
             services.Configure<CookiePolicyOptions>(options =>
             {
-                    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                    options.CheckConsentNeeded = context => true;
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
             services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-                .AddCookie(options =>
-                {
+                .AddCookie(options => {
                     options.AccessDeniedPath = "/user/AccessDenied/";
                     options.LoginPath = "/user/Login/";
                 });
-
             services.AddControllersWithViews(options =>
-            {
-                options.Filters.Add<ViewBagActionFilter>();
-            });
+               {
+                   options.Filters.Add<ViewBagActionFilter>();
+               });
+
         }
+        //Banco de dados na smarterasp
+        //public void ConfigureServices(IServiceCollection services)
+        //{
+        //    var connectionString = "Server=MYSQL5044.site4now.net;Database=db_a9a07d_studytw;Uid=a9a07d_studytw;Pwd=f4487dhwyfdTSDYF";
+        //    var serverVersion = new MySqlServerVersion(new Version(8, 0, 31));
+        //    services.AddDbContext<ApplicationDbContext>(
+        //        dbContextOptions => dbContextOptions
+        //            .UseMySql(connectionString, serverVersion)
+        //            .LogTo(Console.WriteLine, LogLevel.Information)
+        //            .EnableSensitiveDataLogging()
+        //            .EnableDetailedErrors()
+        //    );
+
+        //    services.Configure<CookiePolicyOptions>(options =>
+        //    {
+        //            // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+        //            options.CheckConsentNeeded = context => true;
+        //        options.MinimumSameSitePolicy = SameSiteMode.None;
+        //    });
+
+        //    services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+        //        .AddCookie(options =>
+        //        {
+        //            options.AccessDeniedPath = "/user/AccessDenied/";
+        //            options.LoginPath = "/user/Login/";
+        //        });
+
+        //    services.AddControllersWithViews(options =>
+        //    {
+        //        options.Filters.Add<ViewBagActionFilter>();
+        //    });
+        //}
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
